c769e7416de19eceb41309c2be2bf9a6595c8a6a
betiniakarandut <betiniakarandut@gmail.com>
2023-07-28 13:34:42 +0100

    Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+ #!/usr/bin/python3
+"""Module for Pascal triangle""" 
+
+
+def pascal_triangle(n):
+    """function to represent Pascal triangle
+
+    Args:
+        int(n)
+
+    Returns:
+        list representing pascal triangle
+    """
+
+    if n <= 0:
+        return []
+    trow = [1]
+    y = [0]
+    for x in range(max(n,0)):
+        print(trow)
+        trow=[l+r for l,r in zip(trow+y, y+trow)]
+    return n>=1
+# pascal_triangle(5)
